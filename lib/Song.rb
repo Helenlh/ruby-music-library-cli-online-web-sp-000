4e9c163c493d3a06ac671b4125d00e574990eab6
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-30 01:45:59 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 class Song
-  #attr_accessor :name, :artist, :genre 
   @@all = []
    
    
@@ -10,9 +9,17 @@ class Song
     save 
   end 
   
-   # Classical get method
   
-    
+  def name 
+    @name 
+  end 
+  
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  
   def self.all 
      @@all 
   end 
@@ -22,9 +29,16 @@ class Song
      @@all << self 
   end 
   
+  
+  def create
+    Song.new 
+    Song
+  end 
+  
+  
   def self.new_by_filename(file_name)
     song_name = file_name.split(" - ")[1] 
-    artist_one = file_name.split(" - ")[0]
+    artist_one = file_name.split(" - ") 
     new_song = Song.new(song_name)
     new_song.artist_name=(artist_one)
     new_song
@@ -33,11 +47,11 @@ class Song
   def artist_name=(name) 
       self.artist = Artist.find_or_create_by_name(name)
   end 
-end
+  
+  
+  def self.destroy_all
+    @@all = []
+  end 
+end 
       
       
-    
-    
-    
-    
-    
