822f78015ce9223c977bd053ae862b56dc2ae840
HELEN HARMAN <hlharman@bellsouth.net>
2021-04-02 21:04:18 +0000

    Done.

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,10 +1,12 @@
 class Artist 
+  attr_accessor :name, :song, :artist 
    @@all = [] 
   extend Concerns::Findable
   
+  
   def initialize(name)
     @name = name
-    @song = []  
+    @songs = []  
     save 
   end 
 
@@ -34,22 +36,39 @@ class Artist
   end 
   
   
-  def self.create(name)
-    self.new(name) 
-    return Artist
-  end 
+  def create(name) 
+    artist = Artist.new(name) 
+    save 
+  end
   
   
   def add_song(song)
-    song.artist = self
+    
+  end 
+  
+  
+ def songs   
+   artist.songs << song
+   
+   Song.all.select{|song| song.self == self}
   end 
   
   
   def self.find_by_name(name)
     @@all.find {|person| person.name == name}
   end 
-  
-  
-end #end of class
+end 
+#end of class
 
 
+# def self. describe "#songs" do
+#       it "returns the artist's 'songs' collection (artist has many songs)" do
+#         expect(artist.songs).to eq([])
+
+#         artist.songs << song
+
+#         expect(artist.songs).to include(song)
+#       endcreate(name)
+#     self.new(name) 
+#     return Artist
+#   end 
